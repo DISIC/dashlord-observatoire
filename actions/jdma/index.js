0108ce1735d4b8936508fe3f9387f57cefb758e8
Clément <clement@numericite.eu>
2025-10-17 09:43:05 +0200

    fix: date range 3 month fix range issue

--- actions/jdma/index.js
+++ actions/jdma/index.js
@@ -20,20 +20,24 @@ const getJdmaData = (id, startDate, endDate, fromLastThreeMonth) => {
   let params;
 
   if (JSON.parse(fromLastThreeMonth)) {
-    const threeMonthAgo = new Date(
-      new Date().setMonth(new Date().getMonth() - 3)
-    );
+    let endDate3Month = new Date();
+
+    if (endDate3Month.getTime() > new Date(parseInt(endDate)).getTime()) {
+      endDate3Month = new Date(parseInt(endDate));
+    }
+
+    const threeMonthAgo = new Date(endDate3Month);
+    threeMonthAgo.setMonth(endDate3Month.getMonth() - 3);
     threeMonthAgo.setHours(0, 0, 0, 0);
 
-    const endDateObj = new Date(parseInt(endDate));
-    endDateObj.setHours(23, 59, 59, 999);
+    endDate3Month.setHours(23, 59, 59, 999);
 
     params = {
       input: {
         json: {
           product_id: parseInt(id),
           start_date: threeMonthAgo.getTime().toString(),
-          end_date: endDateObj.getTime().toString(),
+          end_date: endDate3Month.getTime().toString(),
         },
       },
     };
